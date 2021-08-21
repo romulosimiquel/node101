06df361fe05e520ab5e3642ebb7eac1fb54f1dcc
Rômulo Simiquel <romulosimiquel@gmail.com>
2021-08-21 09:02:53 -0300

    primeiro commit

---
+++ data.js
@@ -0,0 +1,9 @@
+const example = {
+    name: "Maria das Graças",
+    email: "mariagraca@gmail.com",
+    senha: "1234",
+  }
+
+const users = [example]
+
+export default { users }  
---
+++ index.js
@@ -0,0 +1,6 @@
+import logger from 'node-color-log'
+
+import data from './data.js';
+
+logger.info("gla gla gla dum!!")
+logger.info(data.users)
